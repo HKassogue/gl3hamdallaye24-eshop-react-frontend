4b29f36e2e140d0977053cb93c565e9572d2cbdf
Dr Hamidou Kassogué <hamidoukass@gmail.com>
2024-07-12 18:55:03 +0000

    Add of content for signin

--- src/components/Connexion.jsx
+++ src/components/Connexion.jsx
@@ -2,9 +2,31 @@ import React from 'react'
 
 function Connexion() {
   return (
-    <div>
-      Dans connexion
-    </div>
+    <main>
+            <div id="auth-form">
+                <h2>Connexion</h2>
+                <form method="post" action="">
+                    <div>
+                        <label for="lo">Login:</label>
+                        <input type="text" name="lo" id="lo"/>
+                    </div>
+                    <div>
+                        <label for="mp">Mot de passe:</label>
+                        <input type="password" name="mp" id="mp"/>
+                    </div>
+                    <div>
+                        <input type="submit" value="Se connecter" class="submit-btn"/>
+                    </div>
+                    <hr/>
+                    <p>
+                        Vous n'avez pas de compte ? <a href="inscription.html">Créez votre compte</a>
+                    </p>
+                </form>
+            </div>
+            <div id="auth-img">
+                <img src={require("../img/auth_img.png")} alt="Authentification"/>
+            </div>
+        </main>
   )
 }
 
